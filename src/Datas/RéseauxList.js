7803f8aa1ad1ebecbe80ee96a808d438cb596546
AdrienD <adrien.deghilage@live.fr>
2022-11-17 15:15:20 +0100

    MAJ site

---
+++ src/Datas/RéseauxList.js
@@ -0,0 +1,24 @@
+import LogoInsta from "../Logos/LogoInsta.png";
+import LogoFB from "../Logos/LogoFB.png";
+import LogoMail from "../Logos/LogoMail.png";
+
+export const ReseauxList = [
+  {
+    name: "Instagram",
+    id: "Res1",
+    icon: LogoInsta,
+    lien: "https://www.instagram.com/jeromejacobsolal/",
+  },
+  {
+    name: "Facebook",
+    id: "Res2",
+    icon: LogoFB,
+    lien: "https://www.facebook.com/profile.php?id=100010539872174",
+  },
+  {
+    name: "Mail",
+    id: "Res3",
+    icon: LogoMail,
+    lien: "",
+  },
+];
